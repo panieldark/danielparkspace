1fb349ef130e96c9dcf4f254e8b32567665891a6
danielpark <danielpark1140@gmail.com>
2021-05-12 20:55:29 -0400

    Got one way down

--- static/js/main.js
+++ static/js/main.js
@@ -5,4 +5,10 @@ function toggleDarkTheme() {
 	element.classList.toggle("dark-mode");
 
 	$(".toggle").toggleClass(["list-group-item", "list-group-item-modified"]);
+
+	// Normal to dark: peach text
+	$(".mr14, .mr145b, h1, h2, h3, h4, h5").toggleClass(["peach"]);
+
+	// Normal to dark: white text
+	$(".summary-text").toggleClass(["white"]);
 }
