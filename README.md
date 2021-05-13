a39a19431e42eb34cfb5528c80518b1754953f1c
danielpark <danielpark1140@gmail.com>
2021-05-12 20:21:18 -0400

    Progress..full color toggle

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ https://danielpark.space
 ## things to add:
 
 - resume info
+- contact info/ways to contact
+  - recaptcha
 - resume pdf view (traditional)
 - light mode / dark mode switch (clean)
 - projects tab
--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,8 @@
 // Dark Theme button toggling
 function toggleDarkTheme() {
 	var element = document.body;
+
 	element.classList.toggle("dark-mode");
+
+	$(".toggle").toggleClass(["list-group-item", "list-group-item-modified"]);
 }
